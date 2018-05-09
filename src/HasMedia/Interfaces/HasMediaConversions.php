e441951b96efbf079b91590163000d2331e9d683
Jens Descamps <jens@digicreate.be>
2018-05-09 09:26:49 +0200

    Minor refactor

--- src/HasMedia/Interfaces/HasMediaConversions.php
+++ src/HasMedia/Interfaces/HasMediaConversions.php
@@ -2,15 +2,17 @@
 
 namespace Spatie\MediaLibrary\HasMedia\Interfaces;
 
+use Spatie\MediaLibrary\Media;
+
 interface HasMediaConversions extends HasMedia
 {
-    
+
     /**
      * Register the conversions that should be performed.
      *
-     * @param $media    The Media Model, base for the conversions
+     * @param Media $media The Media Model, base for the conversions
      *
      * @return array
      */
-    public function registerMediaConversions($media);
+    public function registerMediaConversions(Media $media = null);
 }
